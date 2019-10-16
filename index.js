7f95fe974c5706101d0dc142cc5959ea9b29dc0b
vivien <1456826641@qq.com>
2019-10-16 11:17:33 +0800

    perfect snow codes

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ $(function() {
   }
 
   let snows = document.getElementById("snows");
-  for (let snowIndex = 0; snowIndex < 200; snowIndex++) {
+  for (let snowIndex = 0; snowIndex < 100; snowIndex++) {
     let snow = document.createElement('div');
     snow.className = 'snow';
     snow.style = 
       'left: ' +
-      numberRandom(100, 0) +
+        numberRandom(15, -1) +
       'vw; border: ' +
-      numberRandom(6, 3) +
+        numberRandom(4, 1) +
       'px solid #fff; animation: snowMove ' +
-      numberRandom(5, 3) +
-      's linear infinite;';
+        numberRandom(10, 6) +
+      's linear infinite; animation-delay:' +
+        numberRandom(14, 1) +
+      's';
     snows.appendChild(snow);
   }
 })
